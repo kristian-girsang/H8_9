ea243964e932a0e7a7250e9ac8d08d1eea0d28b6
kristian-girsang <kristian.girsang@ui.ac.id>
2024-02-01 16:01:40 +0700

    upload kode tugas loop

--- gitToday.py
+++ gitToday.py
@@ -1,73 +1,17 @@
-from tabulate import tabulate
+s = 12
 
-name = input('Masukkan Nama Lengkap: ')
-hobby = input('Masukkan hobi anda: ')
-email = input('Masukkan email anda: ')
-nilai_uts = int(input('Masukkan nilai UTS anda: '))
-nilai_uas = int(input('Masukkan nilai UAS anda: '))
+while s <= 19:
+    if s <= 15:
+        print(s)
+    elif s > 18:
+        print('Selesai')
+        print(s)
+    s = s + 1
 
-
-if (nilai_uts + nilai_uas) <= 125 or nilai_uts <= 60 or nilai_uas <= 60:
-    status = 'Gagal'
-else:
-    status = 'Lulus'
-
-
-data = [
-    ["Nama Lengkap", name],
-    ["Hobi", hobby],
-    ["E-mail", email],
-    ["Nilai UTS", nilai_uts],
-    ["Nilai UAS", nilai_uas],
-    ["Status", status]
-]
-
-
-print('\n\n--------------------------')
-print('|   BERIKUT DATA ANDA    |')
-print('--------------------------')
-print(tabulate(data, tablefmt="grid"))
-
-
-
-def to_binary(decimal):
-    binary = ''
-    if decimal == 0:
-        return '0'
-    while decimal > 0:
-        binary = str(decimal %2) + binary
-        decimal = decimal // 2
-    return binary
-
-def to_oktal(decimal):
-    oktal = ''
-    if decimal == 0:
-        return '0'
-    while decimal > 0:
-        oktal = str(decimal % 8) + oktal
-        decimal = decimal // 8
-    return oktal
-
-def to_hexa(decimal):
-    hexa = ''
-    if decimal == 0:
-        return '0'
-    while decimal > 0:
-        hexa = str(decimal % 16) + hexa
-        decimal = decimal // 16
-    return hexa
-
-
-user_input = input('Input: ')
-
-if user_input.isdigit():
-    decimal = int(user_input)
-else:
-    decimal = ord(user_input)
-
-print('Desimal: ', decimal)
-print('Binary: ', to_binary(decimal))
-print('Oktal: ', to_oktal(decimal))
-print('Hexadesimal: ', to_hexa(decimal))
-
-print("Aku males bgt buset")
+# output:
+# 12
+# 13
+# 14
+# 15
+# Selesai
+# 19
